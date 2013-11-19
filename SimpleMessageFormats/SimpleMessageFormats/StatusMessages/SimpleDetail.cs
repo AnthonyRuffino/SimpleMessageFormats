fc3e0cc652b2af83b4064f5ea9789723f03c5344
AnthonyRuffino <aruffino84@gmail.com>
2013-11-19 10:31:50 -0500

    Added SimpleDetail class

---
+++ SimpleMessageFormats/SimpleMessageFormats/StatusMessages/SimpleDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleMessageFormats.StatusMessages
+{
+    class SimpleDetail
+    {
+        public string Guid { get; set; }
+        public int NumericId { get; set; }
+        public string Message { get; set; }
+    }
+}
